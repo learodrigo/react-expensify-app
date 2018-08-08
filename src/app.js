17c1bb8e2280fadea398d842e186abd105647020
Lea Rodrigo <lea.lp@hotmail.com>
2018-08-08 13:41:08 +0200

    Add Header component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -10,7 +10,7 @@ const ExpenseDashboardPage = () => (
   </div>
 );
 
-const AddExpensePage = () => (
+const createExpensePage = () => (
   <div>
     This is from my Add
   </div>
@@ -30,20 +30,34 @@ const HelpPage = () => (
 
 const PageNotFound = () => (
   <div>
-    Error 404 - Page not found :(
+    <h3>Error 404 - Page not found :(</h3>
+    <Link to="/">Dashboard</Link>
   </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Dashboard</Link>
+    <Link to="/create">Create expense</Link>
+    <Link to="/edit">Edit expense</Link>
+    <Link to="/help">Help</Link>
+  </header>
 );
 
 const routes = (
   // BroserRouter has to have ONLY one child
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={PageNotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={createExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
