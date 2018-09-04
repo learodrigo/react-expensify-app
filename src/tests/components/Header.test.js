0ea141d955ed50d125a46f265ce39e0de97ecda7
Lea Rodrigo <lea.lp@hotmail.com>
2018-09-04 20:27:53 +0200

    Dependencies for testing components added

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,1 +1,9 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import Header from '../../components/Header';
+
+test('Should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
